f7ab3ae039e9a1d342ae38739530fc3435015fbb
girmar14 <67840139+webandgis@users.noreply.github.com>
2023-07-07 21:19:09 -0400

    Update Category.jsx

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import ItemListContainer from '../components/ItemListContainer'
 import {ProductsGarza} from '../json/Products'
+import { collection, getDocs, getFirestore } from 'firebase/firestore';
+
 import { useParams } from 'react-router-dom'
 import { CartContext } from '../context/CartContext'
+
 
 const Category = () => {
   const {categoriaId} = useParams()
 
-  const productsGarzaFilter = ProductsGarza.filter(product => product.categoria === categoriaId);
+  const [productsData, setProductsData] = useState([]);
 
-  return <ItemListContainer productsData={productsGarzaFilter} />;
+  useEffect(() => {
+    const db = getFirestore();
+    const productCollection = collection(db, 'products');
+    getDocs(productCollection).then((snapshot) => {
+      const productsGarzaFilter= snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })
+      
+      );
+       setProductsData 
+       (productsGarzaFilter.filter(product => product.categoria === categoriaId));
+    });
+    
+
+  }, []);
+
+
+  
+  return <ItemListContainer productsData={productsData} />;
 };
 export default Category
